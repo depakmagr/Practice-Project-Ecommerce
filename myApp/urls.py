dc09c040319bd7efba21a10404f9fd0a40c78c7d
depakmagr <magardeepak606@gmail.com>
2023-05-16 22:04:53 +0545

    working with templates and setup template

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", home),
-    path("shop/", shop),
+    path("", home, name="index"),
+    path("shop/", shop, name="shop"),
+    path("product-single/", product_single, name="product_single"),
+    path("cart/", cart, name="cart"),
+    path("checkout/", checkout, name="checkout"),
+    path("about/", about, name="about"),
+    path("contact/", contact, name="contact"),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -3,8 +3,25 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1>Hello Deeps!!</h1>")
+    return render(request, template_name="index.html")
 
 
 def shop(request):
-    return HttpResponse("<h1>This is my shop!!</h2>")
+    return render(request, template_name="shop.html")
+
+
+def product_single(request):
+    return render(request, template_name="product_single.html")
+
+def cart(request):
+    return render(request, template_name="cart.html")
+
+def checkout(request):
+    return render(request, template_name="checkout.html")
+
+def about(request):
+    return render(request, template_name="about.html")
+
+def contact(request):
+    return render(request, template_name="contact.html")
+
